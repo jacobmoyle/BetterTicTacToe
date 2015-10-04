f5a148144e5fcf6953e9a6e64215ec2ed781cccb
Jacob Moyle <jacobmoyle@gmail.com>
2015-10-03 21:42:26 -0700

    ai should now return highest scoring index

--- app/game_objects/ai.rb
+++ app/game_objects/ai.rb
@@ -1,37 +1,3 @@
-=begin
-
-It will be assumed that the board will be an array where available moves are empty and taken if containing a string
-
-decide if spot is 'empty'
-- If empty, calculate and save 'score' and spot based on the 'win lines' of the spot
-
-
-### How to calculate score of row ###
-
-ai will have a score keeper for each 'win line'.
-- A B C D E F G H
-
-Row - Indices
-A   - 2,4,6
-B   - 0,4,8
-C   - 1,4,7
-D   - 0,3,6
-E   - 2,5,8
-F   - 0,1,2
-G   - 3,4,5
-H   - 6,7,8
-
-Point opportunities for each row:
-1pt  - blank or only enemy in row
-5pt  - comp + enemy + blank in row
-4pt  - comp + 2 blank in row
-15pt - enemy + enemy + blank in row
-30pt - 2 comp + blank in row
-
-ai will return the index of the best move
-
-=end
-
 class Ai
   attr_accessor :marker, :enemy_marker
 
@@ -56,24 +22,24 @@ class Ai
                           ])
         when 1
           moves[index] = calculate_score([
-                            [board[0],board[1],board[2]], # Row F
+                          [board[0],board[1],board[2]], # Row F
                           [board[1],board[4],board[7]]  # Row C
                           ])
         when 2
           moves[index] = calculate_score([
                           [board[2],board[4],board[6]], # Row A
-                            [board[0],board[1],board[2]], # Row F
+                          [board[0],board[1],board[2]], # Row F
                           [board[2],board[5],board[8]]  # Row E
                           ])
         when 3
           moves[index] = calculate_score([
                           [board[3],board[4],board[5]], # Row G
-                            [board[0],board[3],board[6]]  # Row D
+                          [board[0],board[3],board[6]]  # Row D
                           ])
         when 4
           moves[index] = calculate_score([
                           [board[2],board[4],board[6]], # Row A
-                            [board[0],board[4],board[8]], # Row B
+                          [board[0],board[4],board[8]], # Row B
                           [board[1],board[4],board[7]], # Row C
                           [board[3],board[4],board[5]]  # Row G
                           ])
@@ -85,7 +51,7 @@ class Ai
         when 6
           moves[index] = calculate_score([
                           [board[2],board[4],board[6]], # Row A
-                            [board[0],board[3],board[6]], # Row D
+                          [board[0],board[3],board[6]], # Row D
                           [board[6],board[7],board[8]]  # Row H
                           ])
         when 7
@@ -97,14 +63,16 @@ class Ai
           moves[index] = calculate_score([
                           [board[6],board[7],board[8]], # Row H
                           [board[2],board[5],board[8]], # Row E
-                            [board[0],board[4],board[8]]  # Row B
+                          [board[0],board[4],board[8]]  # Row B
                           ])
         end
+      # If move has already been taken, score is 0
       else
         moves[index] = 0
       end
-    each_with_index
-    puts "ARRAY IS #{moves}"
+    end
+
+    moves.index(moves.max)
   end
 
   private
@@ -135,10 +103,3 @@ class Ai
   end
 
 end
-
-ai = Ai.new(marker:"X", enemy_marker: "O")
-ai.move([
-  0,1,2,
-  3,"X",5,
-  6,7,"O"
-])
