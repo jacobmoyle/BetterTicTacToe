ad3d090b5553f5178b47f42126d51e088033670a
Jacob Moyle <jacobmoyle@gmail.com>
2015-10-05 22:03:59 -0700

    rules class created + tested

---
+++ app/spec/rules_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Rules do
+  #   - dictates if game is a tie
+  #   - dictates if game is over
+  #   - decides who won
+  #   - dictates if a move is invalid
+end
--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require_relative '../game_objects/ai'
 require_relative '../game_objects/human'
+require_relative '../game_objects/rules'
 # require 'human'
