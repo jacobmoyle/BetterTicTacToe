0c082f4a876fe125e20eeed3b751c8b36335d562
Jacob Moyle <jacobmoyle@gmail.com>
2015-10-06 21:45:24 -0700

    replaced double quotes with single

--- app/spec/rules_spec.rb
+++ app/spec/rules_spec.rb
@@ -1,8 +1,1 @@
 require 'spec_helper'
-
-describe Rules do
-  #   - dictates if game is a tie
-  #   - dictates if game is over
-  #   - decides who won
-  #   - dictates if a move is invalid
-end
--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -1,4 +1,3 @@
 require_relative '../game_objects/ai'
 require_relative '../game_objects/human'
 require_relative '../game_objects/rules'
-# require 'human'
