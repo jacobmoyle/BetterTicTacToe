4e260f656ddac37a2ccf4768a5fde0718010d3dd
Jacob Moyle <jacobmoyle@gmail.com>
2015-10-10 19:11:52 -0700

    created an all files to require all files. Cleans up runner a bit

---
+++ app/runner.rb
@@ -0,0 +1,18 @@
+
+
+
+# newgame
+# create players
+#   type of player
+#     name, type, who first, marker
+
+# until game over
+
+#   until player move finalized?
+#     get player move
+#     finalize move
+
+#   switch players
+
+# show winner
+# play again?
